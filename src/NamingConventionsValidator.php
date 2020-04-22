aa0782d5b95c73e720b53900c7ab8a83ed395b75
ivan.lihvar <ivan.lihvar@extreme-idea.com>
2020-04-22 09:07:43 +0300

    put validation to the package

--- src/NamingConventionsValidator.php
+++ src/NamingConventionsValidator.php
@@ -1,57 +1,58 @@
 <?php
 
-
-namespace App;
+namespace Elikh;
 
 use PHPUnit\Framework\TestCase;
 
-
-class NamingConventionsValidator extends TestCase
+abstract class NamingConventionsValidator extends TestCase
 {
-    private $rules = []; //['single'=>'/^[a-z][a-z0-9_]+[^s]$/','nextName'=>'nextRule']
-
-    private $checkedValues = []; //['val1','val2']
-
     private $errors = [];
 
-    public function validate(array $checkedValues, array $rule = ['single' => '/^[a-z][a-z0-9_]+[^s]$/']): string
-    {
-        $this->checkedValues = $checkedValues;
-        $this->addRule($rule);
+    /**
+     * The method returns a list of names to check. ['name1','name2',..]
+     * @return array
+     */
+    abstract public function getNames(): array;
 
-        foreach ($this->checkedValues as $checkedValue) {
-            $this->validateValue($checkedValue);
+    /**
+     * The method returns a list of names to skip checking ['name1','name2',..]
+     * @return array
+     */
+    abstract public function getNamesToSkip(): array;
+
+    /**
+     * The method returns an array of validation rules
+     * like ['single'=>'/^[a-z][a-z0-9_]+[^s]$/','nextName'=>'next PCRE expression']
+     *
+     * @return array
+     */
+    abstract public function getRules(): array;
+
+    public function testTestNameConversation(): void
+    {
+        foreach (array_diff($this->getNames(), $this->getNamesToSkip()) as $checkedName) {
+            $this->validateValue($checkedName);
         }
 
-        return implode(PHP_EOL,$this->errors);
+        $this->assertTrue(empty($errors), implode(PHP_EOL, $this->errors));
     }
 
-    public function addRule(array $rule): void
+    private function validateValue(string $checkedName): void
     {
-        $this->rules = array_merge($this->rules, $rule);
-    }
-
-    public function getRules(): array
-    {
-        return $this->rules;
-    }
-
-    private function validateValue(string $checkedValue): void
-    {
-        foreach ($this->rules as $ruleName => $rule) {
-            if (!$this->check($checkedValue, $rule)) {
-                $this->addError($checkedValue, $ruleName);
+        foreach ($this->getRules() as $ruleName => $rule) {
+            if (!$this->check($checkedName, $rule)) {
+                $this->addError($checkedName, $ruleName);
             }
         }
     }
 
-    private function check(string $checkedValue, string $rule): bool
+    private function check(string $checkedName, string $rule): bool
     {
-        return preg_match($rule, $checkedValue) ? true : false;
+        return preg_match($rule, $checkedName) ? true : false;
     }
 
-    private function addError(string $checkedValue, string $ruleName): void
+    private function addError(string $checkedName, string $ruleName): void
     {
-        $this->errors[] = "Value '$checkedValue' does not match rule '$ruleName'";
+        $this->errors[] = "Value '$checkedName' does not match rule '$ruleName'";
     }
 }
